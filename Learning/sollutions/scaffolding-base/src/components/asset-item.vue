42327ce1f046a32a9073c9f8efd9a69e4c9befae
Tibor Krizsák <TiborK@topdesk.com>
2021-10-29 16:14:35 +0200

    Working on task-01

---
+++ Learning/sollutions/scaffolding-base/src/components/asset-item.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="is-flex is-align-items-center">
+        <div class="media-left">
+            <td-icon icon="add-32" />
+        </div>
+        <div class="media-content is-flex is-flex-direction-column ml-4">
+            <p class="title is-4 has-text-weight-normal has-text-primary">
+                <slot name="title"></slot>
+            </p>
+            <p class="subtitle is-6">
+               <slot name="subtitle"></slot>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import tdIcon from 'src/widgets/td-icon.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    components: { tdIcon },
+});
+</script>
--- Learning/sollutions/scaffolding-base/src/the-app.vue
+++ Learning/sollutions/scaffolding-base/src/the-app.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="page container">
 		<main class="main section">
+			<asset-item>
+				<template v-slot:title>Bob's plant</template>
+				<template v-slot:subtitle>He really likes it.</template>
+			</asset-item>
 		</main>
 	</div>
 </template>
@@ -8,14 +12,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheMessage from './components/the-message.vue';
+import AssetItem from './components/asset-item.vue';
 
 /* scaffolding-enable */
 export default defineComponent({
 	/* scaffolding-disable unless keepExamples */
-	components: { TheMessage },
+	components: { TheMessage, AssetItem },
 	props: {
-		message: { type: String, default: '' },
-	},
+		message: { type: String, default: '' }
+	}
 });
 /* scaffolding-disable unless keepExamples */
 
