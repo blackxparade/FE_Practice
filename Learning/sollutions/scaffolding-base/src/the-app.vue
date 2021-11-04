3f9c281a95a72804c21d6efd9dce81a63abf12c2
laczor <laczor@gmail.com>
2021-11-04 10:37:02 +0100

    Asset-item reviewed

--- Learning/sollutions/scaffolding-base/src/the-app.vue
+++ Learning/sollutions/scaffolding-base/src/the-app.vue
@@ -1,88 +1,94 @@
 <template>
 	<div class="page container">
 		<main class="main section">
-
 			<!-- task-01-asset-item-01 -->
 			<asset-item>
-				<template v-slot:icon>
+				<template #icon>
 					<td-icon :icon="tdExample" />
 				</template>
-				<template v-slot:name>Bob's plant</template>
-				<template v-slot:description>He really likes it.</template>
+				<template #name>Bob's plant</template>
+				<template #description>He really likes it.</template>
 			</asset-item>
-
 			<br>
 
 			<!-- task-01-asset-item-02 -->
 			<asset-item-loc>
-				<template v-slot:icon>
+				<template #icon>
 					<td-icon :icon="tdExample" />
 				</template>
-				<template v-slot:name>Main Stock</template>
-				<template v-slot:location>
+				<template #name>Main Stock</template>
+				<template #location>
 					<a href="http://www.google.com">Stock Room 01</a>
 				</template>
 			</asset-item-loc>
 
+			<h2>Reviewed</h2>
+			<br>
+			<asset-item-review v-bind="sampleAssetItem" />
+			<br>
+			<asset-item-review v-bind="sampleAssetItem">
+				<template #summary>
+					<span>Location: </span> <a href="http://www.google.com">Stock Room 01</a>
+				</template>
+			</asset-item-review>
 			<br>
 
 			<!-- task-02-notification-01 -->
-			<notification notificationType="is-success">
+			<notification notification-type="is-success">
 				You have successfully linked one asset.
 			</notification>
 
-			<notification notificationType="is-warning">
+			<notification notification-type="is-warning">
 				An error occurred while processing your request. Please try again.
 			</notification>
 
-			<notification notificationType="is-danger">
+			<notification notification-type="is-danger">
 				An error occurred while processing your request. Please try again.
 			</notification>
 
 			<!-- task-02-notification-02 -->
-			<notification-enhanced notificationType="is-success">
+			<notification-enhanced notification-type="is-success">
 				Primar lorem ipsum dolor sit amet, consectetur
-  				adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur.
+				adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur.
 			</notification-enhanced>
 
 			<!-- task-03-button -->
-			<bulma-button modifiers="is-warning is-loading mr-4"></bulma-button>
+			<bulma-button modifiers="is-warning is-loading mr-4" />
 
 			<bulma-button modifiers="is-link is-light mr-4" @click="showModal = true">
-				    <span class="icon">
-						<td-icon :icon="tdExample" />
-					</span>
-    			<span>Label</span>
+				<span class="icon">
+					<td-icon :icon="tdExample" />
+				</span>
+				<span>Label</span>
 			</bulma-button>
 
 			<bulma-button modifiers="is-danger is-outlined" @click="showEnhancedModal = true">
-    			<span>Label</span>
-				    <span class="icon">
-						<td-icon :icon="tdExample" />
-					</span>
+				<span>Label</span>
+				<span class="icon">
+					<td-icon :icon="tdExample" />
+				</span>
 			</bulma-button>
 
 			<!-- task-04-modal-01 -->
 			<bulma-modal v-if="showModal" @close="showModal = false">
-				<template v-slot:title>Modal title</template>
-				<template v-slot:content>
+				<template #title>Modal title</template>
+				<template #content>
 					Quisque ante lacus, malesuada ac auctor vitae, congue non ante. Phasellus lacus ex, semper ac tortor nex, fringilla condumentum orci. Fusce eu rutrum tellus.
 				</template>
-				<template v-slot:button-label>Save changes</template>
+				<template #button-label>Save changes</template>
 			</bulma-modal>
 
 			<!-- task-04-modal-02 -->
 			<bulma-modal-enhanced v-if="showEnhancedModal" @close="showEnhancedModal = false">
-				<template v-slot:title>Modal title</template>
-				<template v-slot:content>
+				<template #title>Modal title</template>
+				<template #content>
 					Quisque ante lacus, malesuada ac auctor vitae, congue non ante. Phasellus lacus ex, semper ac tortor nex, fringilla condumentum orci. Fusce eu rutrum tellus.
 				</template>
-				<template v-slot:button-label>Save changes</template>
-				<template v-slot:buttons-custom>
+				<template #button-label>Save changes</template>
+				<template #buttons-custom>
 					<button class="button">Open Asset</button>
 				</template>
 			</bulma-modal-enhanced>
-
 		</main>
 	</div>
 </template>
@@ -91,6 +97,7 @@
 import { defineComponent } from 'vue';
 import TheMessage from './components/the-message.vue';
 import AssetItem from './components/asset-item.vue';
+import AssetItemReview from './components/asset-item-review.vue';
 import AssetItemLoc from './components/asset-item-loc.vue';
 import Notification from './components/notification.vue';
 import NotificationEnhanced from './components/notification-enhanced.vue';
@@ -102,30 +109,33 @@ import { tdExample } from './example-font-awesome-icon-pack';
 /* scaffolding-enable */
 export default defineComponent({
 	/* scaffolding-disable unless keepExamples */
-	components: { 
-		TheMessage, 
-		AssetItem, 
-		AssetItemLoc, 
+	components: {
+		TheMessage,
+		AssetItem,
+		AssetItemReview,
+		AssetItemLoc,
 		Notification,
 		NotificationEnhanced,
 		BulmaButton,
 		BulmaModal,
-		BulmaModalEnhanced
+		BulmaModalEnhanced,
 	},
 	props: {
-		message: { type: String, default: '' }
+		message: { type: String, default: '' },
+	},
+	setup(props) {
+		const sampleAssetItem = { name: 'Item 1', summary: 'Item summary', icon: tdExample, type: 'this is type' };
+		return {
+			tdExample,
+			sampleAssetItem,
+		};
 	},
 	data() {
-    	return { 
+    	return {
 			showModal: false,
-			showEnhancedModal: false 
-		}
+			showEnhancedModal: false,
+		};
   	},
-	setup(props) {
-        return {
-            tdExample
-        };
-    }
 });
 /* scaffolding-disable unless keepExamples */
 
