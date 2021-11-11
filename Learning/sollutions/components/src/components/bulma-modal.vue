94d72cca625fb2729a8466f0b5abf4b5ff597098
laczor <laczor@gmail.com>
2021-11-11 10:24:48 +0100

    Use sr-only instead of aria-label

--- Learning/sollutions/components/src/components/bulma-modal.vue
+++ Learning/sollutions/components/src/components/bulma-modal.vue
@@ -1,39 +1,42 @@
 <template>
-    <div class="modal">
-        <div class="modal-background"></div>
-        <div class="modal-card">
-            <header class="modal-card-head">
-                <p class="modal-card-title">
-                    {{ title }}
-                </p>
-                <button class="delete" aria-label="close"></button>
-            </header>
-            <section class="modal-card-body">
-                <slot name="content">{{ description }}</slot>
-            </section>
-            <footer :class="['modal-card-foot', extraFooterClasses]">
-                <button class="button is-success">
-                    {{ primaryButtonLabel }}
-                </button>
-                <button class="button">
-                    {{ secondaryButtonLabel }}
-                </button>
-                <slot name="extra-buttons"/>
-            </footer>
-        </div>
-    </div>
+	<div class="modal">
+		<div class="modal-background" />
+		<div class="modal-card">
+			<header class="modal-card-head">
+				<p class="modal-card-title">
+					{{ title }}
+				</p>
+				<button class="delete" aria-label="close" />
+				<button class="delete">
+					<span class="sr-only"> Close</span>
+				</button>
+			</header>
+			<section class="modal-card-body">
+				<slot name="content">{{ description }}</slot>
+			</section>
+			<footer :class="['modal-card-foot', extraFooterClasses]">
+				<button class="button is-success">
+					{{ primaryButtonLabel }}
+				</button>
+				<button class="button">
+					{{ secondaryButtonLabel }}
+				</button>
+				<slot name="extra-buttons" />
+			</footer>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default defineComponent({ 
-    props: {
-        title: {type: String, default: ''},
-        description: {type: String, default: ''},
-        primaryButtonLabel: {type: String, default: 'Primary'},
-        secondaryButtonLabel: {type: String, default: 'Secondary'},
-        extraFooterClasses: {type: String, default: ''}
-    }
+export default defineComponent({
+	props: {
+		title: { type: String, default: '' },
+		description: { type: String, default: '' },
+		primaryButtonLabel: { type: String, default: 'Primary' },
+		secondaryButtonLabel: { type: String, default: 'Secondary' },
+		extraFooterClasses: { type: String, default: '' },
+	},
 });
 </script>
